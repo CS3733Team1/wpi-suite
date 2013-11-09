a60e36d2a0193793734ce65cadb12c648532351b
jjbreen <jjbreen@wpi.edu>
2013-11-09 16:13:13 -0500

    commit observer changes

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/AddCommitmentController.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/AddCommitmentController.java
@@ -7,8 +7,6 @@ import edu.wpi.cs.wpisuitetng.modules.calendar.model.CalendarModel;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.Commitment;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.DateTime;
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.CalendarView;
-import edu.wpi.cs.wpisuitetng.modules.postboard.controller.AddMessageRequestObserver;
-import edu.wpi.cs.wpisuitetng.modules.postboard.model.PostBoardMessage;
 import edu.wpi.cs.wpisuitetng.network.Network;
 import edu.wpi.cs.wpisuitetng.network.Request;
 import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
@@ -25,16 +23,17 @@ public class AddCommitmentController implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
+		Commitment commit = new Commitment("Hello", new DateTime());
 		
 		// Send a request to the core to save this message
-		final Request request = Network.getInstance().makeRequest("postboard/postboardmessage", HttpMethod.PUT); // PUT == create
-		request.setBody(new Commitment("Hello", new DateTime()).toJSON()); // put the new message in the body of the request
+		final Request request = Network.getInstance().makeRequest("calendar/commitment", HttpMethod.PUT); // PUT == create
+		request.setBody(commit.toJSON()); // put the new message in the body of the request
 		request.addObserver(new AddCommitmentObserver(this)); // add an observer to process the response
 		request.send(); // send the request
-
+		
 	}
 	
-	public void addCommitmentToModel(){
+	public void addCommitmentToModel(Commitment commit){
 		
 	}
 
