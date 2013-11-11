9f4f350ea6bb5a0f4605eb74316ba882dd3c1875
Kyle Lafontant <klafontant@wpi.edu>
2013-11-11 18:23:44 -0500

    added test package

---
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/CalendarPanelTest.java
@@ -0,0 +1,14 @@
+package edu.wpi.cs.wpisuitetng.modules.calendar.view;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalendarPanelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
