2ffd6966bb1ee17243987cbe7a6d2e4032f2b450
jjbreen <jjbreen@wpi.edu>
2013-11-11 18:24:58 -0500

    fixed server issue

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/CalendarToolBar.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/CalendarToolBar.java
@@ -8,8 +8,11 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import edu.wpi.cs.wpisuitetng.modules.calendar.controller.AddEventController;
 import edu.wpi.cs.wpisuitetng.modules.calendar.controller.RemoveCommitmentController;
+import edu.wpi.cs.wpisuitetng.modules.calendar.controller.RemoveEventController;
 import edu.wpi.cs.wpisuitetng.modules.calendar.controller.display.DisplayCommitmentController;
+import edu.wpi.cs.wpisuitetng.modules.calendar.controller.display.DisplayEventController;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.CalendarModel;
 
 public class CalendarToolBar extends JPanel {
@@ -47,10 +50,12 @@ public class CalendarToolBar extends JPanel {
 		
 		this.setLayout(new BorderLayout());
 		
-		
+		addEventButton.addActionListener(new DisplayEventController(view, model));
+		deleteEventButton.addActionListener(new RemoveEventController(view, model));
 		addCommitmentButton.addActionListener(new DisplayCommitmentController(view, model));
 		deleteCommitmentButton.addActionListener(new RemoveCommitmentController(view, model));
 		
+
 		//addDeletePanel.add(addCalendarButton);
 		//addDeletePanel.add(deleteCalendarButton);
 		addDeletePanel.add(addCommitmentButton);
