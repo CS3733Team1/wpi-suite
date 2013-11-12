51e27134175ed492a46e40bf84a90adc4b6b827b
Chris Molica <cmolica@wpi.edu>
2013-11-12 14:57:09 -0500

    Update Month Calendar Class to change days depending on month, when switching between months

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/MonthCalendar.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/MonthCalendar.java
@@ -41,23 +41,7 @@ public class MonthCalendar extends JFrame {
 	private ArrayList<JPanel> panelList = new ArrayList<JPanel>();
 	private ArrayList<GridBagConstraints> gridBagList = new ArrayList<GridBagConstraints>();
 	private JPanel panel = new JPanel();
-	
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MonthCalendar frame = new MonthCalendar();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
@@ -69,34 +53,38 @@ public class MonthCalendar extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
-		
+
+		int gridSize = 100;
+
 		contentPane.add(panel, BorderLayout.NORTH);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{100, 100, 100, 100, 100, 100, 100};
-		gbl_panel.rowHeights = new int[]{100, 100, 100, 100, 100, 100, 100};
+		gbl_panel.columnWidths = new int[]{gridSize, gridSize, gridSize, gridSize, gridSize, gridSize, gridSize};
+		gbl_panel.rowHeights = new int[]{gridSize, gridSize, gridSize, gridSize, gridSize, gridSize, gridSize};
 		gbl_panel.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
 		gbl_panel.rowWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
 		panel.setLayout(gbl_panel);
-		
-		
-		Calendar mycal = new GregorianCalendar(2013, Calendar.FEBRUARY, 1);
+
+		//TODO: Update with current month and year
+		Calendar mycal = new GregorianCalendar(2013, Calendar.NOVEMBER, 1);
 
 		// Get the number of days in that month
 		int daysInMonth = mycal.getActualMaximum(Calendar.DAY_OF_MONTH); // 28
 		int dayOfWeek = mycal.get(Calendar.DAY_OF_WEEK);
 		mycal.set(Calendar.DAY_OF_MONTH, daysInMonth);
 		int numWeeksMonth = mycal.get(Calendar.WEEK_OF_MONTH);
+
+		if(dayLabel.isEmpty()){
+			addDayLabels();
+			addDays(daysInMonth,dayOfWeek, numWeeksMonth);
+		}
+		else
+		{
+			updateDays(daysInMonth, dayOfWeek, numWeeksMonth);
+		}
 		
-		System.out.println(dayOfWeek);
-		System.out.println(daysInMonth);
-		System.out.println(numWeeksMonth);
-		
-		addDayLabels();
-		addDays(daysInMonth,dayOfWeek, numWeeksMonth);
-		
+
 	}
-	
+
 	public void addDayLabels()
 	{
 
@@ -108,10 +96,10 @@ public class MonthCalendar extends JFrame {
 		gbc_panel_1.gridx = 0;
 		gbc_panel_1.gridy = 0;
 		panel.add(panel_1, gbc_panel_1);
-		
+
 		lblSunday = new JLabel("Sunday");
 		panel_1.add(lblSunday);
-		
+
 		panel_7 = new JPanel();
 		panel_7.setBorder(new LineBorder(new Color(0, 0, 0)));
 		GridBagConstraints gbc_panel_7 = new GridBagConstraints();
@@ -120,10 +108,10 @@ public class MonthCalendar extends JFrame {
 		gbc_panel_7.gridx = 1;
 		gbc_panel_7.gridy = 0;
 		panel.add(panel_7, gbc_panel_7);
-		
+
 		lblMonday = new JLabel("Monday");
 		panel_7.add(lblMonday);
-		
+
 		panel_8 = new JPanel();
 		panel_8.setBorder(new LineBorder(new Color(0, 0, 0)));
 		GridBagConstraints gbc_panel_8 = new GridBagConstraints();
@@ -132,10 +120,10 @@ public class MonthCalendar extends JFrame {
 		gbc_panel_8.gridx = 2;
 		gbc_panel_8.gridy = 0;
 		panel.add(panel_8, gbc_panel_8);
-		
+
 		lblTuesday = new JLabel("Tuesday");
 		panel_8.add(lblTuesday);
-		
+
 		panel_9 = new JPanel();
 		panel_9.setBorder(new LineBorder(new Color(0, 0, 0)));
 		GridBagConstraints gbc_panel_9 = new GridBagConstraints();
@@ -144,11 +132,11 @@ public class MonthCalendar extends JFrame {
 		gbc_panel_9.gridx = 3;
 		gbc_panel_9.gridy = 0;
 		panel.add(panel_9, gbc_panel_9);
-		
+
 		lblWednesday = new JLabel("Wednesday");
 		panel_9.add(lblWednesday);
-		
-		
+
+
 		panel_10 = new JPanel();
 		panel_10.setBorder(new LineBorder(new Color(0, 0, 0)));
 		GridBagConstraints gbc_panel_10 = new GridBagConstraints();
@@ -157,10 +145,10 @@ public class MonthCalendar extends JFrame {
 		gbc_panel_10.gridx = 4;
 		gbc_panel_10.gridy = 0;
 		panel.add(panel_10, gbc_panel_10);
-		
+
 		lblThursday = new JLabel("Thursday");
 		panel_10.add(lblThursday);
-		
+
 		panel_11 = new JPanel();
 		panel_11.setBorder(new LineBorder(new Color(0, 0, 0)));
 		GridBagConstraints gbc_panel_11 = new GridBagConstraints();
@@ -169,10 +157,10 @@ public class MonthCalendar extends JFrame {
 		gbc_panel_11.gridx = 5;
 		gbc_panel_11.gridy = 0;
 		panel.add(panel_11, gbc_panel_11);
-		
+
 		lblFriday = new JLabel("Friday");
 		panel_11.add(lblFriday);
-		
+
 		panel_12 = new JPanel();
 		panel_12.setBorder(new LineBorder(new Color(0, 0, 0)));
 		GridBagConstraints gbc_panel_12 = new GridBagConstraints();
@@ -181,12 +169,12 @@ public class MonthCalendar extends JFrame {
 		gbc_panel_12.gridx = 6;
 		gbc_panel_12.gridy = 0;
 		panel.add(panel_12, gbc_panel_12);
-		
+
 		lblSaturday = new JLabel("Saturday");
 		panel_12.add(lblSaturday);
 	}
-	
-	
+
+
 	public void addDays(int daysInMonth, int dayOfWeekFirstWeek, int numWeeksMonth)
 	{
 		dayOfWeekFirstWeek = dayOfWeekFirstWeek-1;
@@ -198,7 +186,7 @@ public class MonthCalendar extends JFrame {
 		{
 			for(int j = 0; j < 7; j++)
 			{
-				
+
 				if(j == dayOfWeekFirstWeek && !firstDaySet)
 				{
 					firstDaySet = true;
@@ -232,7 +220,7 @@ public class MonthCalendar extends JFrame {
 					sb = sb.append(day);
 					dayLabel.add(new JLabel(sb.toString()));
 					panelList.get(gridIndex).add(dayLabel.get(day-1));
-					
+
 				}
 				else
 				{
@@ -246,7 +234,47 @@ public class MonthCalendar extends JFrame {
 					panel.add(panelList.get(gridIndex), gridBagList.get(gridIndex));
 				}
 				gridIndex++;
-				
+
+			}
+		}
+	}
+	public void updateDays(int daysInMonth, int dayOfWeekFirstWeek, int numWeeksMonth)
+	{
+		dayLabel.clear();
+		dayOfWeekFirstWeek = dayOfWeekFirstWeek-1;
+		boolean firstDaySet = false;
+		int day = 1;
+		int gridIndex = 0;
+		for(int i = 1; i < 6; i++)
+		{
+			for(int j = 0; j < 7; j++)
+			{
+
+				if(j == dayOfWeekFirstWeek && !firstDaySet)
+				{
+					firstDaySet = true;
+					StringBuilder sb = new StringBuilder();
+					sb = sb.append("");
+					sb = sb.append(day);
+					dayLabel.add(new JLabel(sb.toString()));
+					panelList.get(gridIndex).add(dayLabel.get(day-1));
+				}
+				else if(day < daysInMonth && firstDaySet)
+				{
+					day++;
+					StringBuilder sb = new StringBuilder();
+					sb = sb.append("");
+					sb = sb.append(day);
+					dayLabel.add(new JLabel(sb.toString()));
+					panelList.get(gridIndex).add(dayLabel.get(day-1));
+
+				}
+				else
+				{
+
+				}
+				gridIndex++;
+
 			}
 		}
 	}
