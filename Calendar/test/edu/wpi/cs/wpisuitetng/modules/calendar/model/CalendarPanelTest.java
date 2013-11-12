3920e57a76dd6ba26dd5182e8ca8d3b6d5eeb2f1
jjbreen <jjbreen@wpi.edu>
2013-11-11 19:40:09 -0500

    calendar test panel

--- Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/model/CalendarPanelTest.java
+++ Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/model/CalendarPanelTest.java
@@ -1,9 +1,5 @@
-<<<<<<< HEAD:Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/CalendarPanelTest.java
-/*
-package edu.wpi.cs.wpisuitetng.modules.calendar.view;
-=======
 package edu.wpi.cs.wpisuitetng.modules.calendar.model;
->>>>>>> 3b9c4a17cee7220a371537ea90d81f7d2131b87b:Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/model/CalendarPanelTest.java
+
 
 import static org.junit.Assert.*;
 
@@ -17,4 +13,4 @@ public class CalendarPanelTest {
 	}
 
 }
-*/
+
