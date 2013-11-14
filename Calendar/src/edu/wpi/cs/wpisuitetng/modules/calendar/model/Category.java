9b575f1e5294a1b61c1477d685793954f0d3fc19
ztestep <ztestep@github.com>
2013-11-14 18:11:35 -0500

    AJ and I updated Category and created CategoryEntitymanager.

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/Category.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/Category.java
@@ -12,7 +12,9 @@ package edu.wpi.cs.wpisuitetng.modules.calendar.model;
 
 import java.awt.Color;
 
-public class Category {
+import com.google.gson.Gson;
+
+public class Category extends Schedulable {
 	private String name;
 	private Color color;
 	
@@ -38,5 +40,38 @@ public class Category {
 
 	public void setColor(Color color) {
 		this.color = color;
+	}
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		this.markForDeletion();
+	}
+	@Override
+	public String toJSON() {
+		//name, dueDate, description, category
+		String str = new Gson().toJson(this, Category.class);
+		return str;
+	}
+	
+	public static Category[] fromJSONArray(String input)
+	{
+		final Gson parser = new Gson();
+		return parser.fromJson(input, Category[].class);
+	}
+	
+	public static Category fromJSON(String input)
+	{
+		final Gson parser = new Gson();
+		return parser.fromJson(input, Category.class);
+	}
+	@Override
+	public Boolean identify(Object o) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
