21410c94fe18b310c0c2253c6ae3697a3c1d8d4c
jjbreen <jjbreen@wpi.edu>
2013-11-14 19:20:12 -0500

    added mont view items

---
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/calendarview/DatePanel.java
@@ -0,0 +1,82 @@
+package edu.wpi.cs.wpisuitetng.modules.calendar.view.calendarview;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.ListIterator;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
+
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.Event;
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.EventModel;
+
+public class DatePanel extends JPanel implements ListDataListener{
+	private Date paneldate;
+	private ArrayList<JPanel> eventlist;
+	
+	public DatePanel(){
+	}
+	
+	public void setDate(Date today){
+		paneldate = today;
+	}
+	
+	public Date getDate(){
+		return paneldate;
+	}
+	
+	public void addEventPanel(Event eve){
+		JPanel eventpan = new JPanel();
+		eventpan.add(new JLabel(eve.toString()));
+		eventlist.add(eventpan);
+	}
+	
+	public void removeEventPanel(){
+		for (int x = 0; x < eventlist.size(); x++){
+			remove(eventlist.get(x));
+		}
+	}
+
+	@Override
+	public void intervalAdded(ListDataEvent e) {
+		removeEventPanel();
+		ListIterator<Event> event = EventModel.getEventModel().getList().listIterator();
+		while(event.hasNext()){
+			Event eve = event.next();
+			Date evedate = eve.getStartDate();
+			if (evedate.getYear() == paneldate.getYear() && evedate.getDay() == paneldate.getDay() && evedate.getMonth() == paneldate.getMonth()){
+				addEventPanel(eve);
+			}
+		}
+		
+	}
+
+	@Override
+	public void intervalRemoved(ListDataEvent e) {
+		removeEventPanel();
+		ListIterator<Event> event = EventModel.getEventModel().getList().listIterator();
+		while(event.hasNext()){
+			Event eve = event.next();
+			Date evedate = eve.getStartDate();
+			if (evedate.getYear() == paneldate.getYear() && evedate.getDay() == paneldate.getDay() && evedate.getMonth() == paneldate.getMonth()){
+				addEventPanel(eve);
+			}
+		}
+	}
+
+	@Override
+	public void contentsChanged(ListDataEvent e) {
+		removeEventPanel();
+		ListIterator<Event> event = EventModel.getEventModel().getList().listIterator();
+		while(event.hasNext()){
+			Event eve = event.next();
+			Date evedate = eve.getStartDate();
+			if (evedate.getYear() == paneldate.getYear() && evedate.getDay() == paneldate.getDay() && evedate.getMonth() == paneldate.getMonth()){
+				addEventPanel(eve);
+			}
+		}
+	}
+}
