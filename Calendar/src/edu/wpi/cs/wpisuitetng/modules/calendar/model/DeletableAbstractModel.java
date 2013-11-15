799ca193d176bcf5dd86347198e06e880813922d
ztestep <ztestep@github.com>
2013-11-14 19:42:43 -0500

    And more stuff

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/DeletableAbstractModel.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/DeletableAbstractModel.java
@@ -15,7 +15,6 @@ import java.util.Date;
 import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
 
 public abstract class DeletableAbstractModel extends AbstractModel{
-
 	
 	protected boolean isMarkedForDeletion = false;
 	
