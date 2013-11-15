99ffb529b9020710d302050eead73d116daafdc3
ztestep <ztestep@github.com>
2013-11-14 19:07:57 -0500

    Aj and I fixing a whole bunch of problems. DeletableAbstractModel. Category. Category Entity Manager....etc

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/DeletableAbstractModel.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/DeletableAbstractModel.java
@@ -14,13 +14,8 @@ import java.util.Date;
 
 import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
 
-public abstract class Schedulable extends AbstractModel{
-	private String name;
-	private Date dueDate;
+public abstract class DeletableAbstractModel extends AbstractModel{
 
-	// Optional parameters
-	private String description;
-	private Category category;
 	
 	protected boolean isMarkedForDeletion = false;
 	
@@ -32,32 +27,5 @@ public abstract class Schedulable extends AbstractModel{
 	{
 		this.isMarkedForDeletion = true;
 	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Date getDueDate() {
-		return dueDate;
-	}
-	public void setDueDate(Date dueDate) {
-		this.dueDate = dueDate;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public Category getCategory() {
-		return category;
-	}
-	public void setCategory(Category category) {
-		this.category = category;
-	}
-	
-	public int compareTo(Schedulable s){
-		return 0;
-	}
+
 }
