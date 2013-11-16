ff6438b8bbbd271e72a53cf936b36651b0544a5b
Nick Cebry <nick.cebry@gmail.com>
2013-11-16 16:21:36 -0500

    removed ActionListener and KeyListener from DatePicker, were unused remnants from code copied from requirements manager

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/calendarview/DatePicker.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/calendarview/DatePicker.java
@@ -11,17 +11,13 @@
 package edu.wpi.cs.wpisuitetng.modules.calendar.view.calendarview;
 
 import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import org.jdesktop.swingx.JXMonthView;
 import org.jdesktop.swingx.calendar.DateSelectionModel.SelectionMode;
 
 /**
  * date picking calendar
  */
-public class DatePicker extends JXMonthView implements ActionListener, KeyListener{
+public class DatePicker extends JXMonthView{
 
 	public static final Color START_END_DAY = new Color(47, 150, 9);
 	public static final Color SELECTION = new Color(236,252,144);
@@ -52,21 +48,4 @@ public class DatePicker extends JXMonthView implements ActionListener, KeyListen
 		
 		this.setAlignmentX(CENTER_ALIGNMENT);
 	}
-	
-	@Override
-	public void keyTyped(KeyEvent e) {
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-	}
-
 }
