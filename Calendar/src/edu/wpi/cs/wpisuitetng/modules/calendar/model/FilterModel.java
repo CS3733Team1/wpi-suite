85d41d055c6728a4caa1e09e9f1a105e51f48769
Nick Otero <notero87@gmail.com>
2013-11-17 14:33:05 -0500

    Added add and remove functions to FilterModel

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/FilterModel.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/FilterModel.java
@@ -43,7 +43,16 @@ public class FilterModel extends AbstractListModel<Object>
 	public void createFilter(String name, ArrayList<Category> categories)
 	{
 		this.filters.add(new Filter(name, categories));
+		
+		this.fireIntervalAdded(this, 0, 0);
 	}//end createFilter
+	
+	public void addFilter(Filter toAdd)
+	{
+		this.filters.add(toAdd);
+		
+		this.fireIntervalAdded(this, 0, 0);
+	}//end addFilter
 	
 	public void setActiveEventFilter(int filterIndex)
 	{
@@ -70,23 +79,69 @@ public class FilterModel extends AbstractListModel<Object>
 		
 		return this.activeEventFilter.apply(eventArray);
 	}//end applyFilter
-
-	@Override
-	public Object getElementAt(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	/**
+	 * Removes all filters from this model
+	 * 
+	 * NOTE: One cannot simply construct a new instance of the model, because
+	 * other classes in this module have references to it. Hence, we manually
+	 * remove each filter from the model.
+	 */
+	public void emptyModel() 
+	{
+		int oldSize = getSize();
+		Iterator<Filter> iterator = filters.iterator();
+		while (iterator.hasNext()) 
+		{
+			iterator.next();
+			iterator.remove();
+		}
+		this.fireIntervalRemoved(this, 0, Math.max(oldSize - 1, 0));
 	}
 
+	/**
+	 * Returns the filter at the given index. Note this method returns elements in reverse
+	 * order, so newest filters are returned first.
+	 * 
+	 * @see javax.swing.ListModel#getElementAt(int)
+	 */
+	@Override
+	public Object getElementAt(int index) 
+	{
+		return filters.get(filters.size() - 1 - index);
+	}
+	
+	public Object getElement(int index){
+		return filters.get(filters.size() - 1 - index);
+	}
+
+	public void removeFilter(int index) 
+	{
+		filters.remove(index);
+		this.fireIntervalAdded(this, 0, 0);
+	}
+
+	public void removeFilter(Filter filter) 
+	{
+		filters.remove(filter);
+		this.fireIntervalAdded(this, 0, 0);
+	}
+
+	/**
+	 * Returns the number of filters in the model.
+	 * 
+	 * @see javax.swing.ListModel#getSize()
+	 */
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return filters.size();
+	}
+
+	static public List<Filter> getList(){
+		List<Filter> rtnFilterList = new ArrayList<Filter>();
+		rtnFilterList.addAll(getFilterModel().filters);
+		return rtnFilterList;
 	}
 	
 }//end class
-	
-
-	/**
-	 * Constructs a new calendar with no commitments.
-	 */
 	
