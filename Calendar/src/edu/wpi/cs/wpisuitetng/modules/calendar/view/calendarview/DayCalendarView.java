d84853414a48fd2590645dcf7ff9a530c95aea06
Collins <cosei@worcester.edu>
2013-11-16 22:55:41 -0500

    Added columns and time to CalendarDayView(Temporary files)

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/calendarview/DayCalendarView.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/calendarview/DayCalendarView.java
@@ -17,6 +17,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
 import java.util.ArrayList;
@@ -27,6 +28,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
 
 import net.miginfocom.swing.MigLayout;
@@ -44,8 +46,8 @@ public class DayCalendarView extends JPanel implements ICalendarView {
 	private ArrayList<JPanel> panelList = new ArrayList<JPanel>();
 	private ArrayList<JLabel> nameLabelList = new ArrayList<JLabel>();
 	private ArrayList<JPanel> nameList = new ArrayList<JPanel>();
-	private GridLayout gLay = new GridLayout(48, 1, 5, 5);
-	private JPanel contentPane = new JPanel();
+	private JPanel grid1 = new JPanel();
+	private JPanel grid2 = new JPanel();
 
 	private Calendar mycal;
 	private int currentMonth;
@@ -54,21 +56,119 @@ public class DayCalendarView extends JPanel implements ICalendarView {
 	private int currentYear;
 
 	public DayCalendarView() {
-		contentPane.setLayout(gLay);
+		mycal = new GregorianCalendar();
+		currentDay = 1; //mycal.get(Calendar.DAY_OF_MONTH);
+		currentMonth = 3; //mycal.get(Calendar.MONTH);
+		currentYear = 2013; //mycal.get(Calendar.YEAR) + 1900;
+		currentDayOfWeek = 3; // mycal.get(Calendar.DAY_OF_WEEK);
+		
+		grid1.setLayout(new GridLayout(0, 1));
+		grid2.setLayout(new GridLayout(0, 1));
 		
 		JButton b = new JButton("Fake");
 		Dimension buttonSize = b.getPreferredSize();
-		contentPane.setPreferredSize(new Dimension(200, 1000));
+		grid1.setPreferredSize(new Dimension(600, 2000));
+		grid2.setPreferredSize(new Dimension(50, 2000));
 		
-		for(int i = 0; i < 97; i++) {
-			JPanel temp = new JPanel();
-			temp.setBackground(Color.white);
-			contentPane.add(temp);
+		JPanel header1 = new JPanel();
+		JPanel header2 = new JPanel();
+		header1.setBackground(new Color(138, 173, 209));
+		header2.setBackground(new Color(138, 173, 209));
+		header1.add(new JLabel("Events"));
+		header2.add(new JLabel("Time"));
+		
+		grid1.add(header1);
+		grid2.add(header2);
+		
+		grid2.add(new JLabel("0:00 AM"), BorderLayout.PAGE_START);
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("0:30 AM"));
+		grid2.add(new JLabel("1:00 AM"), BorderLayout.PAGE_START);
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("1:30 AM"));
+		grid2.add(new JLabel("2:00 AM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("2:30 AM"));
+		grid2.add(new JLabel("3:00 AM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("3:30 AM"));
+		grid2.add(new JLabel("4:00 AM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("4:30 AM"));
+		grid2.add(new JLabel("5:00 AM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("5:30 AM"));
+		grid2.add(new JLabel("6:00 AM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("6:30 AM"));
+		grid2.add(new JLabel("7:00 AM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("7:30 AM"));
+		grid2.add(new JLabel("8:00 AM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("8:30 AM"));
+		grid2.add(new JLabel("9:00 AM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("9:30 AM"));
+		grid2.add(new JLabel("10:00 AM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("10:30 AM"));
+		grid2.add(new JLabel("11:00 AM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("11:30 AM"));
+		grid2.add(new JLabel("12:00 PM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("12:30 PM"));
+		grid2.add(new JLabel("1:00 PM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("1:30 PM"));
+		grid2.add(new JLabel("2:00 PM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("2:30 PM"));
+		grid2.add(new JLabel("3:00 PM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("3:30 PM"));
+		grid2.add(new JLabel("4:00 PM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("4:30 PM"));
+		grid2.add(new JLabel("5:00 PM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("5:30 PM"));
+		grid2.add(new JLabel("6:00 PM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("6:30 PM"));
+		grid2.add(new JLabel("7:00 PM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("7:30 PM"));
+		grid2.add(new JLabel("8:00 PM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("8:30 PM"));
+		grid2.add(new JLabel("9:00 PM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("9:30 PM"));
+		grid2.add(new JLabel("10:00 PM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("10:30 PM"));
+		grid2.add(new JLabel("11:00 PM"));
+		grid2.add(new JLabel());
+		//grid2.add(new JLabel("11:30 PM"));
+		
+		for(int i = 0; i < 48; i++) {
+			JPanel temp1 = new JPanel();
+			temp1.setBackground(Color.white);
+			temp1.add(new JLabel("JPanel: "+i));
+			if(i%2 == 0)
+				temp1.setBorder(new MatteBorder(0, 0, 2, 0, Color.GRAY));
+			else
+				temp1.setBorder(new MatteBorder(0, 0, 2, 0, Color.BLACK));
+			grid1.add(temp1);
 		}
 		
-		contentPane.setVisible(true);
+		grid1.setVisible(true);
+		grid2.setVisible(true);
 		
-		this.add(contentPane);
+		this.add(grid2);
+		this.add(grid1);
 		
 		/*int gridWidth = 110;
 		int gridHeight = 100;
@@ -131,19 +231,50 @@ public class DayCalendarView extends JPanel implements ICalendarView {
 		dayGridBagConstraint.weighty = 1;
 		dayGridBagConstraint.gridx = 1;
 		dayGridBagConstraint.gridy = 0;
-		contentPane.add(dayPanelLabel,dayGridBagConstraint);
+		grid1.add(dayPanelLabel,dayGridBagConstraint);
 		dayPanelLabel.add(dayJLabel);
 		
 	}
 	@Override
 	public void next() {
-		// TODO Auto-generated method stub
+		Calendar current = Calendar.getInstance();
+		int month = current.get(Calendar.MONTH);
+
+		if (currentMonth == 11 && currentDay == 31){
+			currentMonth = 0;
+			currentDay = 1;
+			currentYear++;
+		} else if(mycal.getActualMaximum(Calendar.DAY_OF_MONTH) == currentDay){
+			currentDay = 1;
+			currentMonth++;
+		}
+		else{
+			currentDay++;
+		}
+		Calendar next = new GregorianCalendar(currentYear, currentMonth, 1);
+		mycal = next;
 
 	}
 
 	@Override
 	public void previous() {
+		Calendar current = Calendar.getInstance();
+		int month = current.get(Calendar.MONTH);
 
+		if (currentMonth == 0 && currentDay == 1){
+			currentMonth = 11;
+			currentDay = 31;
+			currentYear--;
+		} else if(mycal.getActualMinimum(Calendar.DAY_OF_MONTH) == currentDay) {
+			currentMonth--;
+			Calendar next = new GregorianCalendar(currentYear, currentMonth, 1);
+			currentDay = next.getActualMaximum(Calendar.DAY_OF_MONTH);
+		}
+		else{
+			currentDay--;
+		}
+		Calendar next = new GregorianCalendar(currentYear, currentMonth, 1);
+		mycal = next;
 	}
 
 	@Override
