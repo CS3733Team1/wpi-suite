4b097e4bd20ea189eba3ff83326030a016cd8c53
Nick Cebry <nick.cebry@gmail.com>
2013-11-21 21:08:54 -0500

    tooltip expanded to actually display event info instead of just a test message, a little buggy, displays same time for start and end

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/calendarview/week/EventWeekView.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/calendarview/week/EventWeekView.java
@@ -114,7 +114,27 @@ public class EventWeekView extends JPanel{
 				evebuilder.append(",grow, push");
 
 				event.add(new JLabel(e.getName()), "wmin 0, aligny center, alignx center");
-				event.setToolTipText("<html><b>Testing</b></html>");
+				
+				StringBuilder infobuilder = new StringBuilder();
+				infobuilder.append("<html><p><b>Name: </b>");
+				infobuilder.append(e.getName());
+				infobuilder.append("</p><p><b>Start: </b>");
+				infobuilder.append(DateFormat.getInstance().format(e.getStartDate()));
+				infobuilder.append("</p><p><b>End: </b>");
+				infobuilder.append(DateFormat.getInstance().format(e.getEndDate()));
+				infobuilder.append("</p>");
+				if(e.getCategory()!=null){
+					infobuilder.append("<p><b>Category: </b>");
+					infobuilder.append(e.getCategory().getName());
+					infobuilder.append("</p>");
+				}
+				if(e.getDescription()!=null){
+					infobuilder.append("<p><b>Description: </b>");
+					infobuilder.append(e.getDescription());
+					infobuilder.append("</p>");
+				}
+				infobuilder.append("</html>");
+				event.setToolTipText(infobuilder.toString());
 				
 				StringBuilder infobuilder = new StringBuilder();
 				infobuilder.append("<html><p style='width:175px'><b>Name: </b>");
