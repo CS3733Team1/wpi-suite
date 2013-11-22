de0c2a0be3b8671eaffd603ee3d3ca7ec81efc6d
Kyle Lafontant <klafontant@wpi.edu>
2013-11-21 19:21:52 -0500

    add EditCommitmentController class

---
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/commitment/EditCommitmentController.java
@@ -0,0 +1,22 @@
+package edu.wpi.cs.wpisuitetng.modules.calendar.controller.commitment;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.Commitment;
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.CommitmentListModel;
+import edu.wpi.cs.wpisuitetng.modules.calendar.view.commitment.CommitmentTabPanel;
+import edu.wpi.cs.wpisuitetng.network.Network;
+import edu.wpi.cs.wpisuitetng.network.Request;
+import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
+
+
+public class EditCommitmentController implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
