d0813eaf1b08b2a115600550963b61c11056bdb9
Kyle Lafontant <klafontant@wpi.edu>
2013-11-22 18:22:52 -0500

    Update Commitment Model

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/commitment/UpdateCommitmentController.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/commitment/UpdateCommitmentController.java
@@ -10,8 +10,14 @@
 
 package edu.wpi.cs.wpisuitetng.modules.calendar.controller.commitment;
 
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JList;
 
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.Commitment;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.CommitmentListModel;
@@ -21,12 +27,53 @@ import edu.wpi.cs.wpisuitetng.network.Request;
 import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
 
 
-public class UpdateCommitmentController implements ActionListener {
+public class UpdateCommitmentController implements MouseListener{
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+	private JList<Commitment> commitmentList;
+	CommitmentListModel model;
+	CommitmentTabPanel view;
+	
+	public UpdateCommitmentController(CommitmentTabPanel view){
+		this.model = CommitmentListModel.getCommitmentListModel();
+		this.view = view;
+	}
+	
+	public void updateCommitmentModel (Commitment commitment){
 		
 	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (e.getClickCount() == 2) {
+			Rectangle r = commitmentList.getCellBounds(0, commitmentList.getLastVisibleIndex()); 
+			if (r != null && r.contains(e.getPoint())) { 
+				int index = commitmentList.locationToIndex(e.getPoint());
+				System.out.println("Double clicked on Item " + index);
+			}
+		}
+	}
 
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
 }
