d97f98abca4c6d261995490837f2f8ae2f4fb073
Nick Otero <notero87@gmail.com>
2013-11-22 18:28:44 -0500

    Finished updateCommitmentObserver

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/commitment/UpdateCommitmentObserver.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/commitment/UpdateCommitmentObserver.java
@@ -9,12 +9,17 @@
  ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.calendar.controller.commitment;
 
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.Commitment;
+import edu.wpi.cs.wpisuitetng.network.RequestObserver;
+import edu.wpi.cs.wpisuitetng.network.models.IRequest;
+import edu.wpi.cs.wpisuitetng.network.models.ResponseModel;
+
 /**
  * This observer is called when a response is received from a request
  * to the server to update a commitment.
  */
 
-public class UpdateCommitmentObserver 
+public class UpdateCommitmentObserver implements RequestObserver
 {
 	private final UpdateCommitmentController controller;
 	
@@ -25,6 +30,31 @@ public class UpdateCommitmentObserver
 	public UpdateCommitmentObserver(UpdateCommitmentController controller) 
 	{
 		this.controller = controller;
+	}
+	
+	@Override
+	public void responseSuccess(IRequest iReq) 
+	{
+		// Get the response to the given request
+		final ResponseModel response = iReq.getResponse();
+		
+		// Parse the requirement out of the response body
+		final Commitment commitment = Commitment.fromJSON(response.getBody());	
+		// Pass the messages back to the controller
+		controller.updateCommitmentModel(commitment);
+	}
+	
+	@Override
+	public void responseError(IRequest iReq) 
+	{
+		System.err.println("The request to update a commitment failed.");
+		System.err.println("Response: " + iReq.getResponse().getStatusMessage());
+	}
+
+	@Override
+	public void fail(IRequest iReq, Exception exception) 
+	{
+		System.err.println("The request to update a commitment failed.");
 	}
 
 }
