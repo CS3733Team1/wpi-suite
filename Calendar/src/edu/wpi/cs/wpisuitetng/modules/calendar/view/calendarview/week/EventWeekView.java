88be262427ed7fe1fad619dce3b59adbe021ff54
Rohit Bansal <rbansal@wpi.edu>
2013-11-22 18:41:35 -0500

    Fixed showEvent error. Build works now.

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/calendarview/week/EventWeekView.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/calendarview/week/EventWeekView.java
@@ -133,24 +133,6 @@ public class EventWeekView extends JPanel{
 				infobuilder.append("</p></html>");
 				event.setToolTipText(infobuilder.toString());
 				
-				StringBuilder infobuilder = new StringBuilder();
-				infobuilder.append("<html><p style='width:175px'><b>Name: </b>");
-				infobuilder.append(e.getName());
-				infobuilder.append("<br><b>Start: </b>");
-				infobuilder.append(DateFormat.getInstance().format(e.getStartDate()));
-				infobuilder.append("<br><b>End: </b>");
-				infobuilder.append(DateFormat.getInstance().format(e.getEndDate()));
-				if(e.getCategory()!=null){
-					infobuilder.append("<br><b>Category: </b>");
-					infobuilder.append(e.getCategory().getName());
-				}
-				if(e.getDescription()!=null){
-					infobuilder.append("<br><b>Description: </b>");
-					infobuilder.append(e.getDescription());
-				}
-				infobuilder.append("</p></html>");
-				event.setToolTipText(infobuilder.toString());
-				
 				if (e.getCategory() != null){
 					event.setBackground(e.getCategory().getColor());
 				}
