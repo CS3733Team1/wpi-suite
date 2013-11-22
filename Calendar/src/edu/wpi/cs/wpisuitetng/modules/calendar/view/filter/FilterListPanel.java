42a62bd258d52bf2062ceeeb77215e2f60b0b459
Rohit Bansal <rbansal@wpi.edu>
2013-11-21 19:08:31 -0500

    Added FilterListPanel

---
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/filter/FilterListPanel.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2013 WPI-Suite
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Team TART
+ ******************************************************************************/
+
+package edu.wpi.cs.wpisuitetng.modules.calendar.view.filter;
+
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+
+import net.miginfocom.swing.MigLayout;
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.Category;
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.CategoryListModel;
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.Filter;
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.FilterListModel;
+
+/**
+ * This is the view where the list of categories are displayed.
+ * Creates both a scroll pane and JList which stores all of the categories.
+ * @version Revision: 1.0
+ * @author
+ */
+public class FilterListPanel extends JPanel {
+
+	private FilterListModel model;
+	private JList<Filter> filterList;
+	private JScrollPane scrollPane;
+
+	public FilterListPanel() {
+		this.model = FilterListModel.getFilterListModel();
+
+		this.setLayout(new MigLayout("fill, insets 0"));
+
+		filterList = new JList<Filter>(model);
+
+		filterList.setCellRenderer(new FilterListCellRenderer());
+		filterList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+
+		filterList.setLayoutOrientation(JList.VERTICAL);
+
+		filterList.setVisibleRowCount(0);
+
+		scrollPane = new JScrollPane(filterList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+		this.add(scrollPane, "grow, push");
+	}
+
+	public JList<Filter> getCategoryList() {
+		return filterList;
+	}
+
+	public void clearSelection() {
+		filterList.clearSelection();
+	}
+}
