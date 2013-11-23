d17b58047f66ef50fe679bd02edcf686a9d19805
Nick Cebry <nick.cebry@gmail.com>
2013-11-23 15:37:58 -0500

    Added color to background of info panels for events, changed color of text for displayed events and info panel to black or white depending on darkness of background.

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/calendarview/EventHoverMouseListener.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/calendarview/EventHoverMouseListener.java
@@ -8,6 +8,8 @@ import java.text.DateFormat;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
 
 import net.miginfocom.swing.MigLayout;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.Event;
@@ -25,53 +27,26 @@ public class EventHoverMouseListener implements MouseListener{
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		
-		System.out.println("hovering over an event");
-		this.epanel.setBackground(Color.RED);
-		
-		/*
-		int epanelXPos,epanelWidth,epanelYPos,epanelHeight;
-		epanelXPos=epanel.getX();
-		epanelYPos=epanel.getY();
-		epanelWidth=epanel.getWidth();
-		epanelHeight=epanel.getHeight();
-		
-		JPanel infoPanel = new JPanel(new MigLayout());
-		StringBuilder evebuilder = new StringBuilder();
-		evebuilder.append("cell ");
-		evebuilder.append(new Integer(epanelXPos+epanelWidth).toString());
-		evebuilder.append(" ");
-		evebuilder.append(new Integer(epanelYPos).toString());
-		evebuilder.append(" ");
-		evebuilder.append("0");
-		evebuilder.append(" ");
-		evebuilder.append(new Integer(epanelHeight).toString());
-		evebuilder.append(",grow, push");
-		
-		JTextArea eventInfo = new JTextArea();
-		eventInfo.append(this.e.getName());
-		eventInfo.append(DateFormat.getInstance().format(this.e.getStartDate()));
-		eventInfo.append(DateFormat.getInstance().format(this.e.getEndDate()));
-		if(this.e.getCategory() != null){
-			if(this.e.getCategory().getName() != null){
-				eventInfo.append(this.e.getCategory().getName());
+		if (this.e.getCategory() != null){
+			UIManager.put("ToolTip.background",new ColorUIResource(this.e.getCategory().getColor()));
+			float[] hsb=new float[3];
+			Color catColor=this.e.getCategory().getColor();
+			hsb=Color.RGBtoHSB(catColor.getRed(), catColor.getGreen(), catColor.getBlue(), hsb);
+			if(hsb[2]<0.5){
+				UIManager.put("ToolTip.foreground", new ColorUIResource(Color.WHITE));
+			}
+			else{
+				UIManager.put("ToolTip.foreground", new ColorUIResource(Color.BLACK));
 			}
 		}
-		if(this.e.getDescription() != null){
-			eventInfo.append(this.e.getDescription());
+		else{
+			UIManager.put("ToolTip.background", new ColorUIResource(Color.CYAN));
+			UIManager.put("ToolTip.foreground",new ColorUIResource(Color.BLACK));
 		}
-		infoPanel.add(eventInfo);
-		
-		System.out.println("built info panel");
-		
-		calview.add(infoPanel,evebuilder.toString());
-		*/
 	}
 	
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO remove the info panel
-		this.epanel.setBackground(Color.GREEN);
 	}
 
 	@Override
