0346886c5df4e7f99db1a39386c3d0699209db24
Kyle Lafontant <klafontant@wpi.edu>
2013-12-02 20:08:37 -0500

    double click bug fix

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/commitment/CommitmentListPanel.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/commitment/CommitmentListPanel.java
@@ -10,35 +10,26 @@
 
 package edu.wpi.cs.wpisuitetng.modules.calendar.view.commitment;
 
-import java.awt.Component;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JEditorPane;
-import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextPane;
 import javax.swing.ListSelectionModel;
 
 import net.miginfocom.swing.MigLayout;
-import edu.wpi.cs.wpisuitetng.modules.calendar.controller.commitment.AddCommitmentController;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.Commitment;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.CommitmentListModel;
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.CalendarPanel;
-import edu.wpi.cs.wpisuitetng.modules.calendar.view.filter.FilterListPanel;
 
 /**
  * This is the view where the list of commitments are displayed.
@@ -52,12 +43,10 @@ public class CommitmentListPanel extends JPanel implements ActionListener, Mouse
 	private CommitmentListModel model;
 	private CalendarPanel calendarPanel;
 	private JList<Commitment> commitmentList;
-	private Boolean EDITMODE = false;
 	
 	private JEditorPane detailDisplay;
 	private JButton updateCommitmentButton;
 	private JButton cancelButton;
-	private JButton deleteButton;
 	private Commitment selectedCommitment;
 	
 	/**
@@ -68,23 +57,6 @@ public class CommitmentListPanel extends JPanel implements ActionListener, Mouse
 		this.model = CommitmentListModel.getCommitmentListModel();
 		this.calendarPanel = calendarPanel;
 		viewCommitments();
-		/*
-		this.setLayout(new MigLayout("fill, insets 0"));
-		
-		commitmentList = new JList<Commitment>(model);
-		
-		commitmentList.setCellRenderer(new CommitmentListCellRenderer());
-		commitmentList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-
-		commitmentList.setLayoutOrientation(JList.VERTICAL);
-		
-		commitmentList.setVisibleRowCount(0);
-
-		JScrollPane scrollPane = new JScrollPane(commitmentList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-
-		this.add(scrollPane, "grow, push");
-	
-		 commitmentList.addMouseListener(this);*/
 	}
 	
 	/**
@@ -98,7 +70,8 @@ public class CommitmentListPanel extends JPanel implements ActionListener, Mouse
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		   if (e.getClickCount() == 2) {
-	        	 if (commitmentList.contains(e.getPoint())) 
+			   Rectangle r = commitmentList.getCellBounds(0, commitmentList.getLastVisibleIndex());
+	        	 if (r != null && r.contains(e.getPoint())) 
 	        	 { 
 	        		 selectedCommitment = commitmentList.getSelectedValue();
 	        		 editCommitment();
@@ -134,33 +107,7 @@ public class CommitmentListPanel extends JPanel implements ActionListener, Mouse
 		this.add(detailDisplay, "grow, push");
 		
 		
-		
-		/*
-		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		JEditorPane detailDisplay = new JEditorPane("text/html", c.toString());
-		//detailDisplay.setLineWrap(true);
-		//detailDisplay.setWrapStyleWord(true);
-		this.add(detailDisplay);
-		/*this.add(new JLabel("<html>Commitment Name:  " + c.getName() + "</html>"));
-		this.add(new JLabel("Commitment Date:  " + c.getDueDate()));
-		this.add(new JLabel("Commitment Category:  " + c.getCategory().getName()));
-		this.add(new JLabel("<html>Commitment Descrption:  " + c.getDescription() + "</html>"));*/
 		/** Setup gui for editing commitments **/
-		/*
-		// Add / Cancel buttons
-				updateCommitmentButton = new JButton("Update Commitment");
-				updateCommitmentButton.setActionCommand("updatecommitment");
-				//updateCommitmentButton.addActionListener(new AddCommitmentController(this));
-				
-				this.add(updateCommitmentButton, "alignx left, split 2");
-				
-				cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("cancel");
-				
-				this.add(cancelButton, "alignx left");
-				
-				//Action Listener for Cancel Button
-				//cancelButton.addActionListener(this); */
 	}
 	
 	private void viewCommitments()
