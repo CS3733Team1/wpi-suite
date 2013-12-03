28ca2acec00bbdef15366fc966253798f987fb88
A.J. Tamburri <ajtamburri@wpi.edu>
2013-12-03 13:35:49 -0500

    Added setUp() method to Category test case

--- Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/model/CategoryTest.java
+++ Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/model/CategoryTest.java
@@ -2,9 +2,21 @@ package edu.wpi.cs.wpisuitetng.modules.calendar.model;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CategoryTest {
+
+	Category cat1;
+	Category cat2;
+	
+	@Before
+	public void setUp() {
+		cat1 = new Category("School", Color.blue);
+		cat2 = new Category("Work", Color.red);
+	}
 
 	@Test
 	public void test() {
