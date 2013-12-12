0d37e63ec92c4bc06060b9681bd97dbc4ae70812
A.J. Tamburri <ajtamburri@wpi.edu>
2013-12-12 18:34:04 -0500

    Uncommented QuickListPanel

--- Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/view/commitment/CommitmentQuickListTests.java
+++ Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/view/commitment/CommitmentQuickListTests.java
@@ -2,12 +2,22 @@ package edu.wpi.cs.wpisuitetng.modules.calendar.view.commitment;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CommitmentQuickListTests {
 
+	
+	
+	@Before
+	public void setUp()
+	{
+		
+	}
+	
 	@Test
-	public void returnAList() {
+	public void constructedQuicklistIsNotNull()
+	{
 		fail("Not yet implemented.");
 	}
 
