ebc464649d0db75e3df248c4321e6a6c98345d39
jjbreen <jjbreen@wpi.edu>
2013-12-14 14:11:50 -0500

    multiday improvements

---
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/calendarview/day2/DayMultiDayHolder.java
@@ -0,0 +1,9 @@
+package edu.wpi.cs.wpisuitetng.modules.calendar.view.calendarview.day2;
+
+public class DayMultiDayHolder {
+	private MultidayEventView mview;
+	
+	public DayMultiDayHolder(){
+		
+	}
+}
