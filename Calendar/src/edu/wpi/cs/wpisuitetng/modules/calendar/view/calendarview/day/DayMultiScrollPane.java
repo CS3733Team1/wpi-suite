28d5a79d5a389556ef635927c50603ea16683e83
Shadi Ramadan <srramadan@wpi.edu>
2013-12-18 04:24:51 -0500

    Tried to clean up week/day multidayevents

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/calendarview/day/DayMultiScrollPane.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/calendarview/day/DayMultiScrollPane.java
@@ -1,7 +1,5 @@
 package edu.wpi.cs.wpisuitetng.modules.calendar.view.calendarview.day;
 
-import java.awt.Color;
-
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.border.MatteBorder;
@@ -24,9 +22,8 @@ public class DayMultiScrollPane extends JScrollPane{
 		
 		this.setWheelScrollingEnabled(true);
 		this.getVerticalScrollBar().setUnitIncrement(scrollSpeed);
-		
+		this.setBorder(new MatteBorder(0 ,0 ,0 ,0, this.getBackground()));
 		this.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		this.setBorder(new MatteBorder(0, 0, 0, 0, Color.LIGHT_GRAY));
 	}
 
 }
