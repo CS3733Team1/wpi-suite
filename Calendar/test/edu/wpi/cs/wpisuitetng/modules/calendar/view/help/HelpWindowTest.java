10ff336cc03d9280d7a7b18f8940cab33d1039a2
Shadi Ramadan <srramadan@wpi.edu>
2013-12-17 19:20:54 -0500

    renamed event and commitments lists for better tab sizing

--- Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/view/help/HelpWindowTest.java
+++ Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/view/help/HelpWindowTest.java
@@ -1,6 +1,6 @@
 package edu.wpi.cs.wpisuitetng.modules.calendar.view.help;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.After;
 import org.junit.Before;
