4ff2efc62e7916329d14b03633f392bac760b7d3
jjbreen <jjbreen@wpi.edu>
2013-12-17 19:41:35 -0500

    fixed tests

--- Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/view/DayViewTest.java
+++ Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/view/DayViewTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.awt.Color;
 import java.util.Date;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,6 +14,8 @@ import edu.wpi.cs.wpisuitetng.modules.calendar.model.commitment.Commitment;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.commitment.CommitmentListModel;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.event.Event;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.event.EventListModel;
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.filter.FilteredCommitmentsListModel;
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.filter.FilteredEventsListModel;
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.calendarview.day.DayArea;
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.calendarview.day.DayCalendar;
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.calendarview.day.ScheduleItem;
@@ -21,6 +24,25 @@ public class DayViewTest {
 	DayCalendar day;
 	Category cat;
 	
+	@After
+	public void testTearDown(){
+		System.gc();
+		EventListModel emodel = EventListModel.getEventListModel();
+		CommitmentListModel cmodel = CommitmentListModel.getCommitmentListModel();
+		emodel.emptyModel();
+		cmodel.emptyModel();
+		
+		FilteredEventsListModel femodel = FilteredEventsListModel.getFilteredEventsListModel();
+		FilteredCommitmentsListModel fcmodel = FilteredCommitmentsListModel.getFilteredCommitmentsListModel();
+		
+		for (int x = 0; x < femodel.getListDataListeners().length; x++){
+			femodel.removeListDataListener(femodel.getListDataListeners()[x]);
+		}
+		for (int x = 0; x < fcmodel.getListDataListeners().length; x++){
+			fcmodel.removeListDataListener(fcmodel.getListDataListeners()[x]);
+		}
+	}
+	
 	@Before
 	public void setUp() {
 		System.gc();
@@ -28,6 +50,17 @@ public class DayViewTest {
 		CommitmentListModel cmodel = CommitmentListModel.getCommitmentListModel();
 		emodel.emptyModel();
 		cmodel.emptyModel();
+		
+		FilteredEventsListModel femodel = FilteredEventsListModel.getFilteredEventsListModel();
+		FilteredCommitmentsListModel fcmodel = FilteredCommitmentsListModel.getFilteredCommitmentsListModel();
+		
+		for (int x = 0; x < femodel.getListDataListeners().length; x++){
+			femodel.removeListDataListener(femodel.getListDataListeners()[x]);
+		}
+		for (int x = 0; x < fcmodel.getListDataListeners().length; x++){
+			fcmodel.removeListDataListener(fcmodel.getListDataListeners()[x]);
+		}
+		
 		day = new DayCalendar();
 		cat = new Category("This is not Being Test", Color.BLUE);
 	}
