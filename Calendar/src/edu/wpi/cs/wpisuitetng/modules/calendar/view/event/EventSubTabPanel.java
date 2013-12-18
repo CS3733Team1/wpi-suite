f2c3115f7d16d542bef4b2ce6bbb2f68c9608f78
Shadi Ramadan <srramadan@wpi.edu>
2013-12-17 19:09:39 -0500

    Added an All Events List

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/event/EventSubTabPanel.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/event/EventSubTabPanel.java
@@ -8,7 +8,7 @@
  * Contributors: Team TART
  ******************************************************************************/
 
-package edu.wpi.cs.wpisuitetng.modules.calendar.view.commitment;
+package edu.wpi.cs.wpisuitetng.modules.calendar.view.event;
 
 import java.awt.Point;
 import java.util.ArrayList;
@@ -19,46 +19,46 @@ import javax.swing.JPanel;
 import net.miginfocom.swing.MigLayout;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.commitment.Commitment;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.event.Event;
-import edu.wpi.cs.wpisuitetng.modules.calendar.model.filter.FilteredCommitmentsListModel;
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.filter.FilteredEventsListModel;
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.CalendarPanel;
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.list.ListItemListener;
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.list.SRList;
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.utils.RightClickMenu;
 
-public class CommitmentSubTabPanel extends JPanel implements ListItemListener<Commitment> {
+public class EventSubTabPanel extends JPanel implements ListItemListener<Event> {
 	
-	private SRList<Commitment> commitmentList;
+	private SRList<Event> eventList;
 	
 	private CalendarPanel calendarPanel;
 	
-	public CommitmentSubTabPanel(CalendarPanel calendarPanel) {
+	public EventSubTabPanel(CalendarPanel calendarPanel) {
 		this.setLayout(new MigLayout("fill"));
 		this.calendarPanel = calendarPanel;
 		
-		commitmentList = new SRList<Commitment>(FilteredCommitmentsListModel.getFilteredCommitmentsListModel());
-		commitmentList.setListItemRenderer(new CommitmentListItemRenderer());
-		commitmentList.addListItemListener(this);
+		eventList = new SRList<Event>(FilteredEventsListModel.getFilteredEventsListModel());
+		eventList.setListItemRenderer(new EventListItemRenderer());
+		eventList.addListItemListener(this);
 		
-		this.add(commitmentList, "grow");
+		this.add(eventList, "grow");
 	}
 	
-	public List<Commitment> getSelectedCommitments() {
-		return commitmentList.getSelectedItems();
+	public List<Event> getSelectedEvents() {
+		return eventList.getSelectedItems();
 	}
 
 	@Override
-	public void itemsSelected(List<Commitment> listObjects) {}
+	public void itemsSelected(List<Event> listObjects) {}
 
 	@Override
-	public void itemDoubleClicked(Commitment listObject) {}
+	public void itemDoubleClicked(Event listObject) {}
 
 	@Override
-	public void itemFocused(Commitment listObject) {}
+	public void itemFocused(Event listObject) {}
 
 	@Override
-	public void itemRightClicked(Commitment listObject, Point p) {
-		List<Commitment> listofCommitment = commitmentList.getSelectedItems();
-		RightClickMenu menu = new RightClickMenu(new ArrayList<Event>(), listofCommitment, calendarPanel);
-		menu.show(commitmentList, (int)p.getX(), (int)p.getY());
+	public void itemRightClicked(Event listObject, Point p) {
+		List<Event> listofEvent = eventList.getSelectedItems();
+		RightClickMenu menu = new RightClickMenu(listofEvent, new ArrayList<Commitment>(), calendarPanel);
+		menu.show(eventList, (int)p.getX(), (int)p.getY());
 	}
 }
