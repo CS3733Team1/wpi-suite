50fceade1438c2f445b1ece432c7944184db4d82
Rohit Bansal <rbansal@wpi.edu>
2013-12-18 09:49:32 -0500

    Redeploy

--- Core/WPISuite/src/edu/wpi/cs/wpisuitetng/database/DataStore.java
+++ Core/WPISuite/src/edu/wpi/cs/wpisuitetng/database/DataStore.java
@@ -34,7 +34,7 @@ import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 
 public class DataStore implements Data {
 
-	static String WPI_TNG_DB = "WPI_Suite_Team_Tart_Final.db";
+	static String WPI_TNG_DB = "WPI_Suite_Team_Tart_Final_1.db";
 	private static DataStore myself = null;
 	static ObjectContainer theDB;
 	static ObjectServer server;
